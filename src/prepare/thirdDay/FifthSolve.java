f98a2cb171d2da48efd9cab1b6a88425fc96be0f
Tedeeee <gksrmf359@gmail.com>
2024-03-13 22:56:53 +0900

    숫자 카드 나누기

--- src/prepare/thirdDay/FifthSolve.java
+++ src/prepare/thirdDay/FifthSolve.java
@@ -1,13 +1,11 @@
 package prepare.thirdDay;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class FifthSolve {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] arrayA = {10, 17};
-        int[] arrayB = {5, 20};
+        int[] arrayA = {10, 20};
+        int[] arrayB = {5, 17};
 
         s.solution(arrayA, arrayB);
     }
@@ -21,14 +19,31 @@ class Solution {
 
         for (int i = 0; i < arrayA.length; i++) {
             gcdA = gcd(gcdA, arrayA[i]);
-            gcdA = gcd(gcdA, arrayA[i]);
+            gcdB = gcd(gcdB, arrayB[i]);
+        }
+
+        if (division(arrayA, gcdB)) {
+            if (answer < gcdB) answer = gcdB;
+        }
+
+        if (division(arrayB, gcdA)) {
+            if (answer < gcdA) answer = gcdA;
         }
 
         return answer;
     }
 
     public int gcd(int x, int y) {
-        if (x % y == 0) return y;
+        if (y == 0) return x;
         return gcd(y, x % y);
+    }
+
+    public boolean division(int[] arr, int num) {
+        for (int x : arr) {
+            if (x % num == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
