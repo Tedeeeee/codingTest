d54f32332852e92ee13c387b1db4e9c50e0b40a4
Tedeeee <gksrmf359@gmail.com>
2024-03-20 23:29:50 +0900

    DFS하고 BFS 너무 어렵다

---
+++ src/inflean/DFS_BFS/Nine.java
@@ -0,0 +1,32 @@
+package inflean.DFS_BFS;
+
+import java.util.Scanner;
+
+public class Nine {
+
+    static int n, m;
+    static int[] combi;
+
+    public static void DFS(int L, int s) {
+        if (L == m) {
+            for (int x : combi) System.out.print(x + " ");
+            System.out.println();
+        } else {
+            for (int i = s; i <= n; i++) {
+                combi[L] = i;
+                DFS(L + 1, i + 1);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        combi = new int[m];
+
+        DFS(0, 1);
+    }
+}
