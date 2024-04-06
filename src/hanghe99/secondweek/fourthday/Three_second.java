075822f0cee4c01de5d05fe9b609038a5e680328
Tedeeee <gksrmf359@gmail.com>
2024-04-06 17:27:02 +0900

    번호를 착각

--- src/hanghe99/secondweek/fourthday/Three_second.java
+++ src/hanghe99/secondweek/fourthday/Three_second.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Four_second {
+public class Three_second {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(in.readLine());
