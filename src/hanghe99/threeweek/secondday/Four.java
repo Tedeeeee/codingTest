c31bc3c7ee426f40741e7fbe9da4439d654e8a42
Tedeeee <gksrmf359@gmail.com>
2024-04-16 13:10:28 +0900

    문제를 잘 못 올림

--- src/hanghe99/threeweek/secondday/Four.java
+++ src/hanghe99/threeweek/secondday/Four.java
@@ -1,75 +1,122 @@
 package hanghe99.threeweek.secondday;
-import java.util.*;
+
 import java.io.*;
+import java.util.*;
 
 public class Four {
-    static int[] dR = {1, -1, 0, 0};
-    static int[] dC = {0, 0, 1, -1};
 
-    public static void main(String[] args) throws IOException{
+    static int n;
+    static LinkedList<FireBall> map[][];
+    static int dx[] = {-1,-1,0,1,1,1,0,-1};
+    static int dy[] = {0,1,1,1,0,-1,-1,-1};
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
-        int cnt = 0;
+        StringTokenizer stz = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(stz.nextToken());
+        int M = Integer.parseInt(stz.nextToken());
+        int k = Integer.parseInt(stz.nextToken());
+        map = new LinkedList[n][n];
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                map[i][j] = new LinkedList<>();
+        for(int i = 0; i < M; i++) {
+            stz = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(stz.nextToken())-1;
+            int c = Integer.parseInt(stz.nextToken())-1;
+            int m = Integer.parseInt(stz.nextToken());
+            int s = Integer.parseInt(stz.nextToken());
+            int d = Integer.parseInt(stz.nextToken());
+            map[r][c].add(new FireBall(m, d, s));
+        }
 
-        while(true){
-            int N = Integer.parseInt(br.readLine());
-            cnt++;
+        for(int i = 0; i < k; i++)
+            move();
 
-            if(N == 0) break;
+        System.out.println(sum());
+    }
 
-            int[][] cave = new int[N][N];
-            int[][] thiefLoopy = new int[N][N];
+    public static void move() {
+        LinkedList<FireBall> next[][] = new LinkedList[n][n];
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                next[i][j] = new LinkedList<>();
 
-            for(int i = 0; i<N; i++){
-                StringTokenizer st = new StringTokenizer(br.readLine());
-
-                for(int j = 0; j<N; j++) {
-                    cave[i][j] = Integer.parseInt(st.nextToken());
-                    thiefLoopy[i][j] = -1;
-                }
-            }
-
-            Queue<Node> q = new PriorityQueue<>();
-            q.offer(new Node(0, 0, cave[0][0]));
-            thiefLoopy[0][0] = cave[0][0];
-
-            while(!q.isEmpty()){
-                Node a = q.poll();
-
-                if(a.row == N-1 && a.col == N-1){
-                    sb.append("Problem ").append(cnt).append(": ").append(a.loopy).append("\n");
-                    break;
-                }
-
-                for(int i = 0; i<4; i++){
-                    int dr = a.row + dR[i];
-                    int dc = a.col + dC[i];
-
-                    if(dr<0||dc<0||dr>=N||dc>=N)
-                        continue;
-                    if(thiefLoopy[dr][dc] >= a.loopy + cave[dr][dc])
-                        continue;
-
-                    thiefLoopy[dr][dc] = a.loopy + cave[dr][dc];
-                    q.offer(new Node(dr, dc, thiefLoopy[dr][dc]));
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(map[i][j].size() >= 1) {
+                    for(FireBall fb : map[i][j]) {
+                        int distance = fb.s%n;
+                        int x = i + dx[fb.d]*distance;
+                        int y = j + dy[fb.d]*distance;
+                        if(x >= n)
+                            x -= n;
+                        else if(x < 0)
+                            x += n;
+                        if(y >= n)
+                            y -= n;
+                        else if(y < 0)
+                            y += n;
+                        next[x][y].add(new FireBall(fb.m, fb.d, fb.s));
+                    }
                 }
             }
         }
-
-        System.out.print(sb);
-    }
-}
-class Node implements Comparable<Node>{
-    int row, col, loopy;
-
-    Node(int row, int col, int loopy){
-        this.row = row;
-        this.col = col;
-        this.loopy = loopy;
+        map = next;
+        split();
     }
 
-    @Override
-    public int compareTo(Node a){
-        return this.loopy - a.loopy;
+    public static void split() {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(map[i][j].size() >= 2) {
+                    int mSum = 0;
+                    int sSum = 0;
+                    boolean even = true, odd = true;
+
+                    for(FireBall fb : map[i][j]) {
+                        mSum += fb.m;
+                        sSum += fb.s;
+                        if(fb.d % 2 == 0)
+                            odd = false;
+                        else
+                            even = false;
+                    }
+                    int m = mSum / 5;
+                    int s = sSum / map[i][j].size();
+                    map[i][j].clear();
+
+                    if(m > 0) {
+                        for(int a = 0; a < 4; a++) {
+                            if(odd || even)
+                                map[i][j].add(new FireBall(m, 0 + 2*a, s));
+                            else
+                                map[i][j].add(new FireBall(m, 1 + 2*a, s));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static long sum() {
+        long sum = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++){
+                for(FireBall fb : map[i][j])
+                    sum += fb.m;
+            }
+        }
+
+        return sum;
+    }
+
+    static class FireBall{
+        int m, d, s;
+
+        FireBall(int m, int d, int s) {
+            this.m = m;
+            this.d = d;
+            this.s = s;
+        }
     }
 }
