59e18b478adc6fbf9c748c49df52e1ced7652094
Tedeeee <gksrmf359@gmail.com>
2024-06-16 23:54:05 +0900

    java coding test

--- src/baekjoon/chap02/Main.java
+++ src/baekjoon/chap02/Main.java
@@ -1,10 +1,7 @@
 package baekjoon.chap02;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,40 +9,15 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[9];
-
+        Map<Integer, Integer> map = new HashMap<>();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(st.nextToken());
-            arr[i] = num;
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        Arrays.sort(arr);
-
-        // 중요 포인트
-        // 정렬을 했기때문에 규칙이 발생한다
-        // 1. sum이 target보다 작으면 lt를 증가한다
-        // 2. sum이 target보다 크다면 rt를 감소시킨다.
-        // 3. 값이 같다면 더 좁은 상태로 시작한다
-        // -> 여기가 중요한데 어차피 lt와 rt는 더 커지거나 더 작아질 뿐이기 때문이다.
-        // -> 하지만 이는 중복값이 없을때 전제되는 조건이다.
         int target = Integer.parseInt(br.readLine());
-        int count = 0;
-        int lt = 0;
-        int rt = N - 1;
-        while (lt < rt) {
-            int sum = arr[lt] + arr[rt];
-            if (sum == target) {
-                count++;
-                lt++;
-                rt--;
-            } else if (sum > target) {
-                rt--;
-            } else {
-                lt++;
-            }
-        }
-
-        System.out.println(count);
+        if (map.containsKey(target)) System.out.println(map.get(target));
+        else System.out.println(0);
     }
 }
