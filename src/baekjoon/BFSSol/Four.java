44b0269582328b14cc534947666c85c6ddad6b6e
Tedeeee <gksrmf359@gmail.com>
2024-08-16 23:07:10 +0900

    BFS 골드 문제 풀이

---
+++ src/baekjoon/BFSSol/Four.java
@@ -0,0 +1,114 @@
+package baekjoon.BFSSol;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * 지훈이는 벽이거나 불이면 지나갈 수 없다 == . 이여야만 지나갈수 있다.
+ * 만약 가장자리 즉, 현재 위치 좌표가 0 이거나 N , M 이라면 탈출 가능
+ *
+ * 불은 벽이 아니면 전부다 이동 가능
+ *
+ * 중요한 내용은 불과 지훈이가 같이 움직여야한다 == 큐를 두개 사용할 수 있어야 한다.
+ */
+public class Four {
+    static Queue<Node> jihoonQueue = new LinkedList<>();
+    static Queue<Node> fireQueue = new LinkedList<>();
+    static int[] arrowX = {0, 0, 1, -1};
+    static int[] arrowY = {1, -1, 0, 0};
+    static int N, M, count;
+    static char[][] field;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        field = new char[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < M; j++) {
+                char c = s.charAt(j);
+                field[i][j] = c;
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (field[i][j] == 'J') {
+                    jihoonQueue.offer(new Node(i, j));
+                }
+                if (field[i][j] == 'F') {
+                    fireQueue.offer(new Node(i, j));
+                }
+            }
+        }
+
+        BFS();
+    }
+
+    static void BFS() {
+        while (true) {
+            count++;
+            // 불의 확산 실황
+            int fire = fireQueue.size();
+            while (fire > 0) {
+                fire--;
+
+                Node poll = fireQueue.poll();
+
+                for (int i = 0; i < 4; i++) {
+                    int nx = poll.x + arrowX[i];
+                    int ny = poll.y + arrowY[i];
+
+                    if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+                        if (field[nx][ny] == 'J' || field[nx][ny] == '.') {
+                            fireQueue.offer(new Node(nx, ny));
+                            field[nx][ny] = 'F';
+                        }
+                    }
+                }
+            }
+
+            // 지훈이의 위치 상황
+            int jihoon = jihoonQueue.size();
+            while (jihoon > 0) {
+                jihoon--;
+
+                Node poll = jihoonQueue.poll();
+
+                for (int i = 0; i < 4; i++) {
+                    int nx = poll.x + arrowX[i];
+                    int ny = poll.y + arrowY[i];
+
+                    if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+                        System.out.println(count);
+                        return;
+                    }
+
+                    if (field[nx][ny] == '.') {
+                        jihoonQueue.offer(new Node(nx, ny));
+                        field[nx][ny] = 'J';
+                    }
+                }
+            }
+
+            if (jihoonQueue.isEmpty()) {
+                System.out.println("IMPOSSIBLE");
+                return;
+            }
+        }
+    }
+    static class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ src/baekjoon/BFSSol/Three.java
@@ -0,0 +1,84 @@
+package baekjoon.BFSSol;
+
+import java.io.*;
+import java.util.*;
+
+// 7576 토마토
+public class Three {
+    static int[] arrowX = {0, 0, 1, -1};
+    static int[] arrowY = {1, -1, 0, 0};
+    static int N, M, day;
+    static int[][] park;
+    static Queue<Node> queue = new LinkedList<>();
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        park = new int[M][N];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < N; j++) {
+                park[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (park[i][j] == 1) {
+                    queue.offer(new Node(i, j));
+                }
+            }
+        }
+
+        BFS();
+
+        if (checkTomato()) System.out.println(day - 1) ;
+        else System.out.println(-1);
+    }
+
+    static void BFS() {
+        while (!queue.isEmpty()) {
+            Node poll = queue.poll();
+            int nowX = poll.x;
+            int nowY = poll.y;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = nowX + arrowX[i];
+                int ny = nowY + arrowY[i];
+
+                if (nx >= 0 && ny >= 0 && nx < M && ny < N && park[nx][ny] == 0) {
+                    park[nx][ny] = park[nowX][nowY] + 1;
+                    queue.offer(new Node(nx, ny));
+                }
+            }
+        }
+    }
+
+    static boolean checkTomato() {
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if(park[i][j] == 0){
+                    return false;
+                } else {
+                    day = Math.max(day, park[i][j]);
+                }
+            }
+        }
+        return true;
+    }
+
+    static class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
